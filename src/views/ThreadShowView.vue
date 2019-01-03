1bae8ab700356d4f312d0642a038f79c67b3663d
Alfonso Rognoni <alfonsorognoni@gmail.com>
2019-01-02 21:31:18 -0300

    thread list

---
+++ src/views/ThreadShowView.vue
@@ -0,0 +1,18 @@
+<template>
+  <TreadShow />
+</template>
+<script>
+import TreadShow from '../components/ThreadShow'
+export default {
+  name: 'ThreadShowView',
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  components: {
+    TreadShow
+  }
+}
+</script>
